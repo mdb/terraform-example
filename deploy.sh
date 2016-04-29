66e5f0d687adc048ab42047e8d068cea06b1f083
Mike Ball <mikedball@gmail.com>
2016-04-29 15:04:06 -0400

    improve deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ terraform plan
 terraform apply
 
 echo "Setting git user name"
-git config user.name "TravisCI"
+git config user.name $GH_USER_NAME
 
 echo "Setting git user email"
-git config user.email "mikedball@gmail.com"
+git config user.email $GH_USER_EMAIL
 
 echo "Adding git upstream remote"
-git remote add upstream "https://$GH_TOKEN@github.com/mdb/terraform-example.git"
+git remote add upstream "https://$GH_TOKEN@github.com/$GH_REPO.git"
 
 git checkout master
 
@@ -21,7 +21,7 @@ git add .
 
 NOW=$(TZ=America/New_York date)
 
-git commit -m "tfstate as of $NOW \n\n [ci skip]"
+git commit -m "tfstate: $NOW [ci skip]"
 
 echo "Pushing changes to upstream master"
 git push upstream master
